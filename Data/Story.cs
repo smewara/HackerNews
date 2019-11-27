7efd80bd660cbb21df2cc5ff9ba729ab09bd97fd
Swati Mewara <mewara.swati@gmail.com>
2019-11-27 05:34:11 -0800

    Add files via upload

---
+++ Data/Story.cs
@@ -0,0 +1,44 @@
+﻿namespace HackerNews
+{
+    /// <summary>
+    /// The story class
+    /// </summary>
+    public class Story
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// title of the story
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// uri 
+        /// </summary>
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// author
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// score
+        /// </summary>
+        public int Score { get; set; }
+
+        /// <summary>
+        /// comments id
+        /// </summary>
+        public string Comments { get; set; }
+
+        /// <summary>
+        /// rank
+        /// </summary>
+        public int Rank { get; set; }
+
+    }
+}
